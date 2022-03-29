5bec4473b8c56286cd5250deba94e7767a1f4ade
Ruben Faria <rubenfar5@gmail.com>
2022-03-29 13:42:18 +0100

    Create basedados.pl

---
+++ basedados.pl
@@ -0,0 +1,30 @@
+%resort(nome, preçomedio, localizacao[concelho,zona], tipocomida[], estrelas, avaliaçaomedia, interno[], externo[])
+
+resort("Douro Cister Hotel Resort", 358, localizacao['ucanha', norte], [portuguesa], 4, 8.5, ['animais admitidos', 'atividades desportivas', internet, estacionamento, piscina_interior, 'Spa e centro de bem-estar', 'Centro Fitness'], [museu, teatro, catedral, mosteiro])
+resort("Prazer da Natureza Resort & Spa", 178, localizacao['Caminha', norte], [europeia], 4, 8.6, ['atividades desportivas', internet, estacionamento, piscina, piscina_interior, 'Spa e centro de bem-estar', 'Centro Fitness'], [rio, montanha, mar, lago, praia])
+resort("Hotel Parque do Rio", precomedio, localizacao['Esposende', norte], [portuguesa], 3, 7.5, ['atividades desportivas', internet, estacionamento, piscina], [rio, mar, montanha, praia])
+resort("Vale de Campos", 110, localizacao['Penafiel', norte], [portuguesa], -, 8.5, ['animais admitidos', estacionamento, internet, 'Spa e centro de bem-estar', 'Centro Fitness'], [museu])
+
+resort("Bagos do Vilar", precomedio, localizacao['Cadaval', centro], [portuguesa], -, 8.8, ['Spa e centro de bem-estar', piscina, terraco, jardim, internet, estacionamento, 'animais admitidos'], [jardim, museu, parque, montanha])
+resort("Dolce CampoReal Lisboa", 255, localizacao['Turcifal', centro], [italiana, mediterranica, portuguesa, espanhola, 'local', internacional, "latino-americana"], 5, 8.6, [piscina_interior, piscina, 'Spa e centro de bem-estar', 'Centro Fitness', 'atividades desportivas', internet, estacionamento, 'animais admitidos'], [montanha])
+resort("Aqua Village, Health Resort", 437, localizacao['Oliveira do Hospital', centro], [portuguesa], 5, 9.2, ['atividades desportivas', 'Spa e centro de bem-estar', 'Centro Fitness', biblioteca, piscina, piscina_interior, internet, estacionamento], [museu, lago, montanha])
+resort("The Beachfront - Praia D'EL Reu Golf & Beach Resort", 350, localizacao['Casal da Lagoa Seca', centro], [portuguesa, marisco, 'local'], 5, 8.6, ['atividades desportivas', piscina, 'Centro Fitness', internet, estacionamento, 'animais admitidos'], ['campo de golfe', castelo, parque, jardim, terma, museu, praia])
+resort("Ohai Nazaré Outdoor Resort", precomedio, localizacao['Nazaré',centro], [portuguesa, internacional], 3, 8.0, [estacionamento, piscina, 'animais admitidos', 'atividades desportivas', internet, 'Spa e centro de bem-estar', 'Centro Fitness'], [igreja, miradouro, castelo, mosteiro, forte, parque, praia, mar])
+resort("Bakubaki Eco Surg Resort", precomedio, localizacao['Ferrel', centro], [italiana, mediterranica, portuguesa], 4, 8.3, ['Spa e centro de bem-estar', 'atividades desportivas', internet, estacionamento], [forte, praia, 'campo de golfe', parque, museu, terma, jardim])
+resort("West Cliffs Golf Villas", precomedio, localizacao['Casal da Lagoa Seca', centro], [steakhouse, portuguesa], -, 9.0, ['atividades desportivas', internet, estacionamento, piscina], ['campo de golfe', castelo, praia, mar, parque, jardim, terma, museu])
+
+resort("L'AND Vineyards", precomedio, localizacao['Montemor-o-Novo', 'Alentejo'], [portuguesa], 5, 8.8, ['atividades desportivas', internet, estacionamento, piscina, piscina_interior, 'Spa e centro de bem-estar'], [])
+resort("Herdade da Caniceira", precomedio, localizacao['Ervedal', 'Alentejo'], [portuguesa], -, 9.0, ['atividades desportivas', internet, estacionamento, piscina], [lago])
+resort("Pestana Troia Eco-Resort & Residences", precomedio, localizacao['Tróia', 'Alentejo'], [portuguesa, europeia, pizza], 4, 8.1, ['animais admitidos', 'atividades desportivas', internet, estacionamento, piscina, piscina_interior, 'Spa e centro de bem-estar', 'Centro Fitness'], [mar, rio, montanha, praia, 'campo de golfe', parque, museu])
+
+resort("Pedras do Mar Resort & Spa", 260, localizacao['Fenais da Luz', 'Açores'], [portuguesa, internacional], 5, 8.8, ['atividades desportivas', internet, estacionamento, piscina_interior, piscina, 'Spa e centro de bem-estar', 'Centro Fitness'], ['campo de golfe', igreja, lago, santuario, ponte])
+resort("Quinta das Rosas Villas", precomedio, localizacao['Ribeira Grande', 'Açores'], [portuguesa], -, 9.4, ['atividades desportivas', internet, estacionamento, piscina], [ponte, igreja, lago, santuario, ermida, fumarolas, mar, montanha, 'campo de golfe'])
+resort("Aldeia das Adegas", precomedio, localizacao['São Roque do Pico', 'Açores'], [portuguesa], 4, 8.9, [internet, estacionamento], [])
+resort("Aldeia da Fonte Hotel", precomedio, localizacao['Lajes do Pico', 'Açores'], [portuguesa, internacional], 4, 8.4, ['animais admitidos', 'atividades desportivas', internet, estacionamento, piscina, 'Centro Fitness'], [mar, praia])
+resort("Caparica Azores Ecolodge", 345, localizacao['Biscoitos', 'Açores'], [portuguesa], -, 9.2, ['atividades desportivas', internet, estacionamento, piscina], [mar, montanha, praia])
+
+resort("Saccharum - Resort & Spa - Savoy Signature", 308, localizacao['Calheta', 'Madeira'], [internacional], 5, 9.2, ['atividades desportivas', internet, estacionamento, piscina_interior, piscina, 'Spa e centro de bem-estar', 'Centro Fitness'], [gruta, piscina, mar, montanha, praia])
+resort("Escarpa - The Madeira Hideaway", precomedio, localizacao['Ponta do Sol', 'Madeira'], [mediterranica, portuguesa, marisco, 'local'], -, 9.7, [internet, estacionamento, piscina], [gruta, miradouro, casino, parque, marina, praia])
+resort("Hotel Apartamento Cais da Oliveira", precomedio, localizacao['Caniço', 'Madeira'], [portuguesa], 4, 8.2, ['atividades desportivas', internet, estacionamento, piscina_interior, piscina, 'Spa e centro de bem-estar', 'Centro Fitness'], [praia, quinta, 'campo de golfe', museu, casino, jardim, marina, miradouro])
+resort("Pestana Colombos Premium Clube - All inclusive", 745, localizacao['Porto Santo', 'Madeira'], [portuguesa], 5, 8.6, ['atividades desportivas', internet, estacionamento, piscina_interior, piscina, 'Spa e centro de bem-estar', 'Centro Fitness'], [quinta, praia])
+
