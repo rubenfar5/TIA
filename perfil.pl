946c7768def2c6b4552aba30f0684ed2d7afbcd1
anabelamarin <anabelamarin01@gmail.com>
2022-04-13 00:00:12 +0100

    Update perfil.pl

--- perfil.pl
+++ perfil.pl
@@ -3,7 +3,7 @@
 membro(X, [X|_]).
 membro(X, [_|R]):- membro(X, R).
 
-
+% Praia, cultura ou natureza?
 procuraPraia(L):- membro(praia,L); membro(marina,L).
 
 procuraNatureza(L):- membro(montanha,L); membro(rio,L); membro(mar,L); membro(lago,L); membro(miradouro,L).
@@ -14,7 +14,12 @@ procuraCultura(L):- membro(museu,L); membro(igreja,L); membro(teatro,L); membro(
 decisaoPreferencia(L,U):- (membro(praia, L), procuraPraia(U)); (membro(natureza, L), procuraNatureza(U)); (membro(cultura, L), procuraCultura(U)).
     					
 
-sugestaoHotelPraia(L):- setof(Y,(resort(Y,_,_,_,_,_,_,U), decisaoPreferencia(L,U)),K), print(K).  
+sugestaoHotel(L):- setof(Y,(resort(Y,_,_,_,_,_,_,U), decisaoPreferencia(L,U)),K), print(K).
+
+%localização
+listarHoteisRegiao(N):- setof(Y,(resort(Y,_,L,_,_,_,_,_), membro(N,L)),K), print(K).
+
+%Tenciona levar animais
 
 procuraAnimais(L):- membro('animais admitidos',L).
 
@@ -22,11 +27,15 @@ preferenciaAnimal(L,A):- (membro(sim, L), procuraAnimais(A)).
 
 animaisHotel(L):- setof(Y,(resort(Y,_,_,_,_,_,A,_), preferenciaAnimal(L,A)),K), print(K).
 
+%Gatronomia
+
 procuraComida(L):- membro(europeia,L); membro(italiana,L); membro(mediterranica,L); membro(espanhola,L); membro('local',L); membro(internacional,L); membro(latino-americana,L); membro(marisco,L); membro(steakhouse,L); membro(pizza,L); membro(asiatica,L); membro(saudavel,L); membro(churrasco,L); membro(brasileira,L); membro(inglesa,L).
 
 preferenciaComida(L,A):- (membro(fa, L), procuraComida(A)).
 
 gastronomiaHotel(L):- setof(Y,(resort(Y,_,_,A,_,_,_,_), preferenciaComida(L,A)),K), print(K).
+
+%Desporto
 
 procuraDesporto(L):- membro('atividades desportivas',L); membro('centro fitness',L).
 
