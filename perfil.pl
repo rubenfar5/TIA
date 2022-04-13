97c6ba8da601e920acb9dcfb4f1a170f21f90b7c
Ruben Faria <rubenfar5@gmail.com>
2022-04-13 01:11:33 +0100

    boa sorte

--- perfil.pl
+++ perfil.pl
@@ -1,23 +1,27 @@
-%perfil(zona,perfil)
-
 membro(X, [X|_]).
 membro(X, [_|R]):- membro(X, R).
 
-% Praia, cultura ou natureza?
+%Praia, cultura ou natureza?
+
 procuraPraia(L):- membro(praia,L); membro(marina,L).
 
 procuraNatureza(L):- membro(montanha,L); membro(rio,L); membro(mar,L); membro(lago,L); membro(miradouro,L).
 
 procuraCultura(L):- membro(museu,L); membro(igreja,L); membro(teatro,L); membro(catedral,L); membro(mosteiro,L); membro(castelo,L); membro(santuario,L).
 
-
 decisaoPreferencia(L,U):- (membro(praia, L), procuraPraia(U)); (membro(natureza, L), procuraNatureza(U)); (membro(cultura, L), procuraCultura(U)).
     					
-
 sugestaoHotel(L):- setof(Y,(resort(Y,_,_,_,_,_,_,U), decisaoPreferencia(L,U)),K), print(K).
 
+
 %localização
+
+%procuraLocalidade(L):- membro('Algarve' , L).
+
+%decisaoLocal(N,L):- (membro(1, N), procuraLocalidade(L)).
+
 listarHoteisRegiao(N):- setof(Y,(resort(Y,_,L,_,_,_,_,_), membro(N,L)),K), print(K).
+
 
 %Tenciona levar animais
 
@@ -27,6 +31,7 @@ preferenciaAnimal(L,A):- (membro(sim, L), procuraAnimais(A)).
 
 animaisHotel(L):- setof(Y,(resort(Y,_,_,_,_,_,A,_), preferenciaAnimal(L,A)),K), print(K).
 
+
 %Gatronomia
 
 procuraComida(L):- membro(europeia,L); membro(italiana,L); membro(mediterranica,L); membro(espanhola,L); membro('local',L); membro(internacional,L); membro(latino-americana,L); membro(marisco,L); membro(steakhouse,L); membro(pizza,L); membro(asiatica,L); membro(saudavel,L); membro(churrasco,L); membro(brasileira,L); membro(inglesa,L).
@@ -34,6 +39,7 @@ procuraComida(L):- membro(europeia,L); membro(italiana,L); membro(mediterranica,
 preferenciaComida(L,A):- (membro(fa, L), procuraComida(A)).
 
 gastronomiaHotel(L):- setof(Y,(resort(Y,_,_,A,_,_,_,_), preferenciaComida(L,A)),K), print(K).
+
 
 %Desporto
 
@@ -44,5 +50,8 @@ preferenciaDesporto(L,A):- (membro(desportista, L), procuraDesporto(A)).
 atividadesHotel(L):- setof(Y,(resort(Y,_,_,_,_,_,A,_), preferenciaDesporto(L,A)),K), print(K).
 
 
+verResort(N,G,D,PCN,A):- setof(Y,(resort(Y,_,L,G1,_,_,AD,PCN1), membro(N,L), decisaoPreferencia(PCN,PCN1),  preferenciaAnimal(A,AD), preferenciaComida(G,G1), preferenciaDesporto(D,AD)),K), print(K).
+verResort(G,D,PCN,A):- setof(Y,(resort(Y,_,_,G1,_,_,AD,PCN1), decisaoPreferencia(PCN,PCN1), preferenciaAnimal(A,AD), preferenciaComida(G,G1), preferenciaDesporto(D,AD)),K), print(K).                               
+%verResort([fa], [desportista], [praia, cultura], [sim], [1]).
 
-
+perfil(1,L):- verResort([fa], [desportista], [natureza, cultura], [sim], L).
